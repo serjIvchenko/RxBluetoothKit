d9396b476e49ac2f513ab23985af9fb47dfddbc9
serjIvchenko <Ti4wpRWub2mw2Lm>
2024-06-19 15:01:29 +0300

    add minor fix

--- Source/Characteristic.swift
+++ Source/Characteristic.swift
@@ -6,45 +6,49 @@ import CoreBluetooth
 public class Characteristic {
     /// Intance of CoreBluetooth characteristic class
     public let characteristic: CBCharacteristic
-
+    
     /// Service which contains this characteristic
     public let service: Service
-
+    
     /// Current value of characteristic. If value is not present - it's `nil`.
     public var value: Data? {
         return characteristic.value
     }
-
+    
     /// The Bluetooth UUID of the `Characteristic` instance.
     public var uuid: CBUUID {
         return characteristic.uuid
     }
-
+    
     /// Flag which is set to true if characteristic is currently notifying
     public var isNotifying: Bool {
         return characteristic.isNotifying
     }
-
+    
     /// Properties of characteristic. For more info about this refer to [CBCharacteristicProperties](https://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBCharacteristic_Class/#//apple_ref/c/tdef/CBCharacteristicProperties)
     public var properties: CBCharacteristicProperties {
         return characteristic.properties
     }
-
+    
     /// Value of this property is an array of `Descriptor` objects. They provide more detailed information about characteristics value.
     public var descriptors: [Descriptor]? {
         return characteristic.descriptors?.map { Descriptor(descriptor: $0, characteristic: self) }
     }
-
+    
     init(characteristic: CBCharacteristic, service: Service) {
         self.characteristic = characteristic
         self.service = service
     }
-
-    convenience init(characteristic: CBCharacteristic, peripheral: Peripheral) {
-        let service = Service(peripheral: peripheral, service: characteristic.service)
+    
+    convenience init?(characteristic: CBCharacteristic, peripheral: Peripheral) {
+        guard let cbService = characteristic.service else {
+            return nil
+        }
+        
+        let service = Service(peripheral: peripheral, service: cbService)
         self.init(characteristic: characteristic, service: service)
     }
-
+    
     /// Function that triggers descriptors discovery for characteristic.
     /// - returns: `Single` that emits `next` with array of `Descriptor` instances, once they're discovered.
     ///
@@ -60,7 +64,7 @@ public class Characteristic {
     public func discoverDescriptors() -> Single<[Descriptor]> {
         return service.peripheral.discoverDescriptors(for: self)
     }
-
+    
     /// Function that allow to observe writes that happened for characteristic.
     /// - Returns: `Observable` that emits `next` with `Characteristic` instance every time when write has happened.
     /// It's **infinite** stream, so `.complete` is never called.
@@ -77,7 +81,7 @@ public class Characteristic {
     public func observeWrite() -> Observable<Characteristic> {
         return service.peripheral.observeWrite(for: self)
     }
-
+    
     /// Function that allows to know the exact time, when isNotyfing value has changed on a characteristic.
     ///
     /// - returns: `Observable` emitting `Characteristic` when isNoytfing value has changed.
@@ -94,7 +98,7 @@ public class Characteristic {
     public func observeNotifyValue() -> Observable<Characteristic> {
         return service.peripheral.observeNotifyValue(for: self)
     }
-
+    
     /// Function that triggers write of data to characteristic. Write is called after subscribtion to `Observable` is made.
     /// Behavior of this function strongly depends on [CBCharacteristicWriteType](https://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBPeripheral_Class/#//apple_ref/swift/enum/c:@E@CBCharacteristicWriteType), so be sure to check this out before usage of the method.
     /// - parameter data: `Data` that'll be written to the `Characteristic`
@@ -120,7 +124,7 @@ public class Characteristic {
     public func writeValue(_ data: Data, type: CBCharacteristicWriteType) -> Single<Characteristic> {
         return service.peripheral.writeValue(data, for: self, type: type)
     }
-
+    
     /// Function that allow to observe value updates for `Characteristic` instance.
     /// - Returns: `Observable` that emits `Next` with `Characteristic` instance every time when value has changed.
     /// It's **infinite** stream, so `.complete` is never called.
@@ -137,7 +141,7 @@ public class Characteristic {
     public func observeValueUpdate() -> Observable<Characteristic> {
         return service.peripheral.observeValueUpdate(for: self)
     }
-
+    
     /// Function that triggers read of current value of the `Characteristic` instance.
     /// Read is called after subscription to `Observable` is made.
     /// - Returns: `Single` which emits `next` with given characteristic when value is ready to read.
@@ -154,7 +158,7 @@ public class Characteristic {
     public func readValue() -> Single<Characteristic> {
         return service.peripheral.readValue(for: self)
     }
-
+    
     /// Setup characteristic notification in order to receive callbacks when given characteristic has been changed.
     /// Returned observable will emit `Characteristic` on every notification change.
     /// It is possible to setup more observables for the same characteristic and the lifecycle of the notification will be shared among them.
